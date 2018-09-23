7acdb345cb1eddbb000f8f2b6b676b8368f0637e
happyu <290945266@qq.com>
2018-09-23 21:53:47 +0800

    feat: 完成首页

---
+++ src/componetns/feed/feed.js
@@ -0,0 +1,57 @@
+import Taro, {Component} from "@tarojs/taro";
+import PropTypes from 'prop-types'
+import {View, Image, Text} from "@tarojs/components";
+
+import more from '../../assets/images/more.png'
+
+import './feed.styl'
+
+export default class Feed extends Component {
+  render() {
+    const {feed} = this.props
+    return (
+      <View className='feed-item'>
+        <View className='feed-source'>
+          <View className='avatar flex1'>
+            <Image src={feed.feed_source_img} />
+          </View>
+          <View className='flex8'>
+            <Text className='feed-source-text'>{feed.feed_source_name}{feed.feed_source_txt}</Text>
+          </View>
+          <View className='flex1'>
+            <Image className='item-more' mode='aspectFit' src={more} />
+          </View>
+        </View>
+        <View className='feed-content'>
+          <View className='question'>
+            <View className='question-link'>
+              <Text>{feed.question}</Text>
+            </View>
+          </View>
+          <View className='answer-body'>
+            <View>
+              <Text className='answer-txt'>{feed.answer_ctnt}</Text>
+            </View>
+            <View className='answer-actions'>
+              <View>
+                <View className='link'>{feed.good_num} 赞同</View>
+                <View className='dot m_r_10'></View>
+              </View>
+              <View>
+                <View className='comments'>{feed.comment_num} 评论</View>
+                <View className='dot m_r_10'></View>
+              </View>
+              <View className='follow-it'>
+                <View>关注问题</View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+Feed.propTypes = {
+  feed: PropTypes.object
+}
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,6 +2,7 @@ import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 
 import Search from '../../componetns/search/search'
+import Feed from '../../componetns/feed/feed'
 
 import './index.styl'
 
@@ -11,9 +12,31 @@ export default class Index extends Component {
     navigationBarTitleText: '首页'
   }
 
+  constructor () {
+    super(...arguments)
+    this.state = {
+      loading: true,
+      list: []
+    }
+  }
+
   componentWillMount () { }
 
-  componentDidMount () { }
+  componentDidMount () {
+    // 获取远程数据
+    Taro.showLoading({ title: '加载中' })
+    Taro.request({
+      url: 'https://easy-mock.com/mock/5b21d97f6b88957fa8a502f2/example/feed'
+    }).then(res => {
+      if (res.data.success) {
+        Taro.hideLoading()
+        this.setState({
+          loading: false,
+          list: res.data.data
+        })
+      }
+    })
+  }
 
   componentWillUnmount () { }
 
@@ -22,10 +45,16 @@ export default class Index extends Component {
   componentDidHide () { }
 
   render () {
+    const { loading, list } = this.state
+    console.log(loading, list)
     return (
       <View className='index'>
         <Search />
-        <Text>首页</Text>
+        <View>
+          {list.map(item => (
+            <Feed feed={item} key={item.question_id} />
+          ))}
+        </View>
       </View>
     )
   }
